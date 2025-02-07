0d60dead824862cde676acd1a5aa5d77ec89ebe4
asdzxcvbn1509a <141426904+asdzxcvbn1509a@users.noreply.github.com>
2025-02-07 21:46:33 +0700

    Create X2.cpp

---
+++ X2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int first, second, sum, result;
+    cin >> first >> second;
+    sum = second - first;
+    result = second+sum;
+    cout << result;
+    return 0;
+}
